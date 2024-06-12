227428b4a4cdd3b82a680d251057961a5cd55ff1
Richard White <richard.white@qmul.ac.uk>
2024-06-12 15:58:34 +0100

    Create script to subset count file to expts
    Takes a file of expt names to samples and a all count file and
    creates a directory for each experiment, and outputs a file of
    counts aggregated to the gene level for the samples in each expt.

---
+++ scripts/subset-by-expt.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+
+''' Script to take a DETCT counts file, subset to required samples
+and aggregate the count data from regions to genes '''
+
+import argparse
+import polars.selectors as cs
+import polars as pl
+import re
+import gzip
+import os
+from itertools import repeat
+
+def output_aggregate_counts_for_expt(expt_name, df, sample_info):
+    ''' Function to subset the data frame to just the counts for an 
+    experiment aggregate the counts from the transcript to the gene 
+    level and output to a file '''
+    # get samples for expt
+    sample_names = sample_info.filter(expt = expt_name
+        ).select("sample").with_columns(pl.Series("suffix", [" count"])
+        ).select(pl.concat_str(pl.all())
+        ).to_series(0).to_list()
+    # select columns by name
+    subset = df.select(cs.by_name(df.columns[0:15]) | 
+            cs.by_name(sample_names)
+        ).filter(pl.col("GeneID").str.contains("ENSDARG") & 
+        ~pl.col("GeneID").str.contains(",")
+        ).select(~cs.contains("3' end read count")
+        ).group_by(pl.col("GeneID")
+        ).agg(
+            cs.ends_with("count").sum()
+        )
+    if not os.path.exists(expt_name):
+        os.mkdir(expt_name)
+    outfile = os.path.join(expt_name, "counts-by-gene.tsv")
+    subset.write_csv(outfile, separator = "\t")
+
+def main(args):
+    ''' Main body of code '''
+
+    # read in samples file
+    sample_info = pl.read_csv(args.sample_file, separator="\t")
+    expts = sample_info.unique(subset = "expt"
+        ).select("expt"
+        ).to_series(0).to_list()
+
+    # read in input
+    if re.search("\\.gz$", args.count_file):
+        fh = gzip.open(args.count_file)
+    else:
+        fh = open(args.count_file)
+
+    all_data = pl.read_csv(fh, infer_schema_length = args.infer_schema_length)
+
+    col_map = {}
+    for col_name in all_data.columns:
+        if re.match("e[0-9]+ Ensembl Gene ID", col_name):
+            col_map[col_name] = "GeneID"
+
+    if col_map:
+        all_data = all_data.rename(col_map)
+
+    for expt in expts:
+        output_aggregate_counts_for_expt(expt, all_data, sample_info)
+        if args.verbose:
+            print(expt)
+
+if __name__ == '__main__':
+    desc = ''' Script to take a DETCT counts file, subset to required samples
+    and aggregate the count data from regions to genes '''
+    parser = argparse.ArgumentParser(description = desc)
+    parser.add_argument('sample_file', nargs='?', metavar='SAMPLES',
+        type=str, default='all.csv', help='Samples file name')
+    parser.add_argument('count_file', nargs='?', metavar='COUNTS',
+        type=str, default='all.csv', help='Counts file name')
+    parser.add_argument('--infer_schema_length', default=1000, type=int,
+        metavar = "ROWS",
+        help=''.join(['Alter the number of rows to use to infer the data ',
+        'schema [default: %(default)s]']))
+    parser.add_argument('--verbose', action='store_true', default=False,
+        help='Turns on verbose output')
+    parser.add_argument('--debug', action='count', default=0,
+        help='Prints debugging information')
+    params = parser.parse_args()
+    main(params)
