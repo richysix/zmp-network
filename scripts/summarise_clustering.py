50ee2e44f6656bb3f44abc13cf189346b00e7c8c
Richard White <richard.white@qmul.ac.uk>
2024-07-25 11:30:38 +0100

    Add output of multiple plots to pdf

--- scripts/summarise_clustering.py
+++ scripts/summarise_clustering.py
@@ -5,6 +5,7 @@ import sys
 import polars as pl
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
 
 def main(args):
     ''' Main body of code '''
@@ -12,6 +13,7 @@ def main(args):
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     # read in clustering file
     node_ids = []
+    cluster_ids = []
     cluster_sizes = []
     header = True
     with open(args.input_file, mode = "r") as fh:
@@ -29,6 +31,7 @@ def main(args):
                     if line[0] in numbers:
                         # cluster start
                         cluster_id = ids.pop(0)
+                        cluster_ids.append(cluster_id)
                     node_ids.extend(ids)
                     cluster_sizes.append(len(node_ids))
                     node_ids = []
@@ -36,28 +39,54 @@ def main(args):
                     # cluster start
                     ids = line.split()
                     cluster_id = ids.pop(0)
+                    cluster_ids.append(cluster_id)
                     node_ids.extend(ids)
                 else:
                     ids = line.split()
                     node_ids.extend(ids)
     # summarise clusters
-    df = pl.DataFrame({"cluster_sizes": cluster_sizes})
-    cl_size_fn = args.input_file + ".cl-sizes.csv"
+    df = pl.DataFrame({
+        "cluster_id": cluster_ids,
+        "cluster_size": cluster_sizes 
+    })
+    cl_size_fn = args.input_file + ".cl-sizes.tsv"
+    df.write_csv(cl_size_fn, separator = "\t")
+    no_singletons = df.filter(pl.col("cluster_size") > 1)
+
+    cl_size_fn = args.input_file + ".cl-size-summary.tsv"
     pl.DataFrame({
-        "min": df.select(pl.col("cluster_sizes").min()),
-        "min_gt1": df.filter(pl.col("cluster_sizes") > 1).select(pl.col("cluster_sizes").min()),
-        "q1": df.select(pl.col("cluster_sizes").quantile(0.25)),
-        "median": df.select(pl.col("cluster_sizes").median()),
-        "q3": df.select(pl.col("cluster_sizes").quantile(0.75)),
-        "max": df.select(pl.col("cluster_sizes").max()),
-        "mean": df.select(pl.col("cluster_sizes").mean()),
-        "std": df.select(pl.col("cluster_sizes").std()),
+        "min": df.select(pl.col("cluster_size").min()),
+        "q1": df.select(pl.col("cluster_size").quantile(0.25)),
+        "median": df.select(pl.col("cluster_size").median()),
+        "q3": df.select(pl.col("cluster_size").quantile(0.75)),
+        "max": df.select(pl.col("cluster_size").max()),
+        "mean": df.select(pl.col("cluster_size").mean().round(2)),
+        "std": df.select(pl.col("cluster_size").std().round(2)),
+        "min_gt1": no_singletons.select(pl.col("cluster_size").min()),
+        "q1_gt1": no_singletons.select(pl.col("cluster_size").quantile(0.25)),
+        "median_gt1": no_singletons.select(pl.col("cluster_size").median()),
+        "q3_gt1": no_singletons.select(pl.col("cluster_size").quantile(0.75)),
+        "max_gt1": no_singletons.select(pl.col("cluster_size").max()),
+        "mean_gt1": no_singletons.select(pl.col("cluster_size").mean().round(2)),
+        "std_gt1": no_singletons.select(pl.col("cluster_size").std().round(2)),
     }).write_csv(cl_size_fn, separator = "\t")
 
     # plot histogram of cluster sizes
-    hist = sns.displot(df, x="cluster_sizes", binwidth = 10)
+    hist = sns.displot(df, x="cluster_size", binwidth = 10)
     cl_hist_fn = args.input_file + ".clusters.hist.png"
     hist.savefig(cl_hist_fn)
+
+    # plot histogram with singletons removed
+    # displot throws an error if there is only one value
+    if no_singletons.height <= 1:
+        print("There aren't enough clusters larger than 1 to plot a histogram",
+            file = sys.stderr)
+    else:
+        cl_hist_fn = args.input_file + ".clusters.hist.pdf"
+        with PdfPages(cl_hist_fn) as pdf_pages:
+            for bin_w in [10, 1]:
+                hist = sns.displot(no_singletons, x="cluster_size", binwidth = bin_w)
+                pdf_pages.savefig()
 
 if __name__ == '__main__':
     desc = ''' Script to summarise the cluster sizes from an MCL clustering'''
